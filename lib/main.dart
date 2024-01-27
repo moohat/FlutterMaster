339e880922ea872d03dd11e9b412c61e094fddbb
moohat <refixzcorp@gmail.com>
2024-01-27 23:51:31 +0700

    this.keyword

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,14 @@
+import 'package:first_app/gradient_container.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        // appBar: AppBar(
-        //   title: Text("First App"),
-        // ),
-        // backgroundColor: Color.fromARGB(255, 19, 201, 233),
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(colors: [
-              Colors.amberAccent,
-              Color.fromARGB(255, 116, 49, 241)
-            ]),
-          ),
-          child: const Center(
-            child: Text(
-              "Hello World",
-              style: TextStyle(fontSize: 30, color: Colors.black),
-            ),
-          ),
+        body: GradientContainer(
+          Colors.amberAccent,
+          Color.fromARGB(255, 116, 49, 241),
         ),
       ),
     ),
---
+++ lib/styled_text.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class StyledText extends StatelessWidget {
+  const StyledText(this.text, {super.key});
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Text(text,
+        style: const TextStyle(
+          fontSize: 40,
+          fontWeight: FontWeight.bold,
+        ));
+  }
+}
